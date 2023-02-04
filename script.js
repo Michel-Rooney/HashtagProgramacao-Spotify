a9ab74fcb20d56955823387338522431ec1e4d3e
Michel-Rooney <michelrooney16@gmail.com>
2023-02-03 23:41:55 -0300

    Aula 06

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
 function playSong(){
+    play.querySelector('.bi').classList.replace('bi-play-circle-fill', 'bi-pause-circle-fill');
     song.play();
+    isPlaying = true;
 }
 
-const songName = document.getElementById('song-name');
-const song = document.getElementById('audio');
-const play = document.getElementById('play');
+function pauseSong(){
+    play.querySelector('.bi').classList.replace('bi-pause-circle-fill', 'bi-play-circle-fill');
+    song.pause();
+    isPlaying = false;
+}
 
-play.addEventListener('click', playSong)
+function playPauseDecider(){
+    isPlaying ? pauseSong() : playSong();
+}
+
+function initializeSong(){
+    cover.src = `img/${playlist[index].file}.png`;
+    song.src = `songs/${playlist[index].file}.mp3`;
+    songName.innerHTML = playlist[index].songName;
+    bandName.innerHTML = playlist[index].artist;
+}
+
+function previousSong(){
+    index = index === 0 ? playlist.length - 1 : index -= 1;
+    initializeSong();
+    playSong();
+}
+
+function nextSong(){
+    index = index === playlist.length -1 ? index = 0 : index += 1;
+    initializeSong();
+    playSong();
+}
+
+
+const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
+const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
+const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+const spirit = {
+    songName: 'S.P.I.R.I.T.',
+    artist: 'Bring Me The Horizon',
+    file: 's.p.i.r.i.t'
+}
+const crawling = {
+    songName: 'Crawling',
+    artist: 'Linkin Park',
+    file: 'crawling'
+}
+const enchanted = {
+    songName: 'Enchanted',
+    artist: 'Taylor Swift',
+    file: 'enchanted'
+}
+const playlist = [spirit, crawling, enchanted]
+
+let isPlaying = false;
+let index = 0;
+
+initializeSong();
+
+play.addEventListener('click', playPauseDecider);
+previous.addEventListener('click', previousSong);
+next.addEventListener('click', nextSong);
